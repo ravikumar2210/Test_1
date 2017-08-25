670d81919e30f92051125ec93045323fa78da3ae
ROHIT <ROHIT@s5ystem>
2017-08-25 16:10:20 +0530

    FIRST COMMIT

---
+++ src/SeleniumDemo.java
@@ -0,0 +1,15 @@
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.*;
+
+public class SeleniumDemo {
+
+	public static void main(String[] args) {
+		
+		WebDriver driver = new FirefoxDriver();
+		driver.get("http://google.com");
+		
+
+		
+	}
+
+}
